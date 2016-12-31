6c93de78f68f733041d524ede7a572c3843e3309
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2016-12-31 11:41:17 +0900

    Finish duplicate

---
+++ TodoController.js
@@ -0,0 +1,23 @@
+function TodoController() {
+    var self = this;
+
+    self.todos = [
+        {title: 'システム企画', done: true},
+        {title: '要件定義', done: false}
+    ];
+
+    self.create = function () {
+        self.todos.push({title: self.newTodo, done: false});
+        self.newTodo = '';
+    };
+
+    self.archive = function () {
+        var currentTodo = self.todos;
+        self.todos = [];
+        angular.forEach(currentTodo, function (todo) {
+            if (!todo.done) {
+                self.todos.push(todo);
+            }
+        });
+    };
+}
--- script.js
+++ script.js
@@ -1,24 +1,4 @@
 angular.module('ToDo', [])
-    .controller('TodoController', function () {
-        var self = this;
-
-        self.todos = [
-            {title: 'システム企画', done: true},
-            {title: '要件定義', done: false}
-        ];
-
-        self.create = function () {
-            self.todos.push({title: self.newTodo, done: false});
-            self.newTodo = '';
-        };
-
-        self.archive = function () {
-            var currentTodo = self.todos;
-            self.todos = [];
-            angular.forEach(currentTodo, function (todo) {
-                if(!todo.done) {
-                    self.todos.push(todo);
-                }
-            });
-        };
-    });
+    .controller('TodoController', TodoController)
+    .service('todoService', TodoService)
+    .filter('customFilter', CustomFilter);
